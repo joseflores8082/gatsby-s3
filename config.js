dbeda569050de812a8abc245007e04122f1c68b0
Jose <jf@enterprisealumni.com>
2019-06-11 14:47:45 -0700

    updating cognito

---
+++ config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_LPe0FY88V',
+        userPoolClientId: '6umv2ogio2nnm9fjk0r1s7d09u',
+        region: 'us-west-2'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
+
